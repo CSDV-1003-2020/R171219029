c590bf552802f10c379ae3bf32d37c7d702d8f2b
Pravahini-Sharma <60136152+Pravahini-Sharma@users.noreply.github.com>
2020-04-06 22:12:29 +0530

    updated

--- deletion.cpp
+++ deletion.cpp
@@ -49,5 +49,35 @@ void deleteNode(struct Node **head_ref, int key)
     prev->next = temp->next; 
   
     free(temp);  // Free memory 
-}   
+}  
+// This function prints contents of linked list starting from  
+// the given node 
+void printList(struct Node *node) 
+{ 
+    while (node != NULL) 
+    { 
+        printf(" %d ", node->data); 
+        node = node->next; 
+    } 
+} 
+  
+/* Drier program to test above functions*/
+int main() 
+{ 
+    /* Start with the empty list */
+    struct Node* head = NULL; 
+  
+    push(&head, 7); 
+    push(&head, 1); 
+    push(&head, 3); 
+    push(&head, 2); 
+  
+    puts("Created Linked List: "); 
+    printList(head); 
+    deleteNode(&head, 1); 
+    puts("\nLinked List after Deletion of 1: "); 
+    printList(head); 
+    return 0; 
+}
+
   
