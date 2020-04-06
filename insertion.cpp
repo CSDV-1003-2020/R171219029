2b8eacfc84e73b0f669900d8cf2156030877ce1c
rajatrawatcensored <53485618+rajatrawatcensored@users.noreply.github.com>
2020-04-06 21:39:04 +0530

    reference pointer to pointer

--- insertion.cpp
+++ insertion.cpp
@@ -52,3 +52,48 @@ void insertAfter(Node* prev_node, int new_data)
     /* 5. move the next of prev_node as new_node */
     prev_node->next = new_node;  
 }  
+ /* Given a reference (pointer to pointer) to the head  
+of a list and an int, appends a new node at the end */
+void append(Node** head_ref, int new_data)  
+{  
+    /* 1. allocate node */
+    Node* new_node = new Node(); 
+  
+    Node *last = *head_ref; /* used in step 5*/
+  
+    /* 2. put in the data */
+    new_node->data = new_data;  
+  
+    /* 3. This new node is going to be  
+    the last node, so make next of  
+    it as NULL*/
+    new_node->next = NULL;  
+  
+    /* 4. If the Linked List is empty, 
+    then make the new node as head */
+    if (*head_ref == NULL)  
+    {  
+        *head_ref = new_node;  
+        return;  
+    }  
+  
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL)  
+        last = last->next;  
+  
+    /* 6. Change the next of last node */
+    last->next = new_node;  
+    return;  
+}  
+  
+// This function prints contents of 
+// linked list starting from head  
+void printList(Node *node)  
+{  
+    while (node != NULL)  
+    {  
+        cout<<" "<<node->data;  
+        node = node->next;  
+    }  
+}  
+  
